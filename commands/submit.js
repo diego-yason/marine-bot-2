f25395b37bd8a0611f9f93d872d3f90ebb8f61d7
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2020-12-28 02:17:53 +0800

    Fixed formatting of the table

--- commands/submit.js
+++ commands/submit.js
@@ -1,12 +1,18 @@
+/*
+Required parameters:
+commands
+callback
+*/
+
 module.exports = {
     commands: ["submit"],
-    args: "<bill url> <name>",
-    permissionError: "You need to be a member of any of the authorized roles according to the Congressional Code.",
-    minArgs: 1,
-    maxArgs: 2,
     callback: (message, arguments, text) => {
         // TODO:Add accept message
     },
+    expectedArgs: "<bill url> <name [optional]>",
+    permissionError: "You need to be a member of any of the authorized roles according to the Congressional Code.",
+    minArgs: 1,
+    maxArgs: 2,
     permissions: [],
-    rolePermission: [],
+    rolePermission: ["Federalist Representative", "Supreme Chancellor of the Federalist Republic"],
 }
--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,18 @@
+/*
+Required parameters:
+commands
+callback
+*/
+
 module.exports = {
     commands: ["test", "ping"],
-    callback: 
+    callback: (message, arguments, text) => {
+        // put your code here
+    },
+    expectedArgs: "",
+    permissionError: "",
+    minArgs: 0,
+    maxArgs: null,
+    permissions: [], // discord permissions, check discordpermissions.txt for the list
+    rolePermission: [],
 };
