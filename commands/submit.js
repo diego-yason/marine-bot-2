5e4f503cfa90673714a468e75f8558b1b1cdf8b8
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2020-12-28 18:37:36 +0800

    added callback, temporarily removed role reqs

--- commands/submit.js
+++ commands/submit.js
@@ -4,15 +4,33 @@ commands
 callback
 */
 
+const mongo = require("../mongo");
+const schema = require("../schemas/submit");
+
 module.exports = {
     commands: ["submit"],
-    callback: (message, arguments, text) => {
-        // TODO:Add accept message
+    callback: async (message, arguments, text) => {
+        const { member } = message;
+
+        await mongo().then(async mongoose => {
+            try {
+                await new schema({
+                    _number: "1000",
+                    url: "test",
+                    primary_sponsor: "TestUser",
+                });
+                console.log("Added new data");
+            } catch (e) {
+                console.log("wait nevermined, theres an error" + e);
+            } finally {
+                mongoose.connection.close();
+            }
+        });
     },
     expectedArgs: "<bill url> <name [optional]>",
     permissionError: "You need to be a member of any of the authorized roles according to the Congressional Code.",
     minArgs: 1,
     maxArgs: 2,
     permissions: [],
-    rolePermission: ["Federalist Representative", "Supreme Chancellor of the Federalist Republic"],
-}
+    rolePermission: [],
+};
