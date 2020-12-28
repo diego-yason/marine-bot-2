d423aa6f1d99166c8f89592a377f6fcf8bef26f8
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2020-12-28 19:23:36 +0800

    Added returns

--- _command_handler.js
+++ _command_handler.js
@@ -66,6 +66,7 @@ module.exports = (client, commandOptions) => {
     } catch {
         console.log(`REGISTRATION FAILED | FILE: ${commands[0]}`);
     }
+
     // permissions
     if (permissions.length) {
         if (typeof permissions === "string") {
@@ -76,6 +77,7 @@ module.exports = (client, commandOptions) => {
     }
 
     client.on("message", message => {
+
         // I was today years old (December 27, 2020) that you can do this
         // makes my life WAYYYY easier
         const { member, content, guild } = message;
@@ -90,6 +92,7 @@ module.exports = (client, commandOptions) => {
                 for (const permission of permissions) {
                     if (!member.hasPermission(permission)) {
                         message.reply(permissionError);
+                        return;
                     }
                 }
 
@@ -100,11 +103,13 @@ module.exports = (client, commandOptions) => {
                     // if the role doesn't exist
                     if (!role) {
                         message.reply("Error 1: Role does not exist, ask your admin to add the role required.");
+                        return;
                     }
 
                     // the role exists, but the member doesn't have it
                     if (!member.roles.cache.has(role.id)) {
                         message.reply("Error 2: You do not have the required role.");
+                        return;
                     }
                 }
 
@@ -117,11 +122,13 @@ module.exports = (client, commandOptions) => {
                 // not enough args
                 if (args.length < minArgs) {
                     message.reply(`Error 3: You did not give enough arguments.\n Correct usage: \`${PREFIX}${alias} ${expectedArgs}\``);
+                    return;
                 }
 
                 // too many args
                 if (maxArgs != null && args.length > maxArgs) {
                     message.reply(`Error 4: You have too many arguments.\n Correct usage: \`${PREFIX}${alias} ${expectedArgs}\``);
+                    return;
                 }
 
                 // command
