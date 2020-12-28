80a2a9c38c811d38d8d5d2b5025055e4bd11cf88
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2020-12-28 23:31:15 +0800

    Added register command, for national id

---
+++ commands/register.js
@@ -0,0 +1,18 @@
+/*
+Required parameters:
+commands
+callback
+*/
+
+module.exports = {
+    commands: ["register"],
+    callback: (message, arguments, text) => {
+        
+    },
+    expectedArgs: "",
+    permissionError: "",
+    minArgs: 0,
+    maxArgs: null,
+    permissions: [], // discord permissions, check discordpermissions.txt for the list
+    rolePermission: [],
+}
