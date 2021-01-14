0a0fd260359918164f5a8078c9cb560705c054a1
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-01-14 23:58:08 +0800

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -7,12 +7,10 @@ callback
 module.exports = {
     commands: ["register"],
     callback: (message, arguments, text) => {
-        
+        // code
     },
     expectedArgs: "",
     permissionError: "",
     minArgs: 0,
     maxArgs: null,
-    permissions: [], // discord permissions, check discordpermissions.txt for the list
-    rolePermission: [],
-}
+};
