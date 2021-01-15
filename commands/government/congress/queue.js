963efa24d02021aa70136c606c3c5f282cc73a54
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-01-15 10:18:56 +0800

    Update queue.js

--- commands/government/congress/queue.js
+++ commands/government/congress/queue.js
@@ -1,7 +1,6 @@
 module.exports = {
     commands: ["queue"],
     callback: (message, arguments, text) => {
-        // put your code here
+        //
     },
-    expectedArgs: "No arguments needed in this command.",
 };
