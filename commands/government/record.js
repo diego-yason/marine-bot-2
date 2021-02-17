e24aa3f042c8d7d519e6ebd389ae54ab7f54eb49
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-02-18 02:19:00 +0800

    Update record.js

--- commands/government/record.js
+++ commands/government/record.js
@@ -1,7 +1,20 @@
+const rfr = require("rfr");
+
+const mongo = rfr("mongo.js");
+
+// REMINDME to switch require to rfr again
+const MarineClass = require("../../constructors");
+
 module.exports = {
     commands: ["record"],
+    expectedArgs: "k!record <department>",
     callback: (message, args, text) => {
-        
+        const newDoc = new MarineClass.GovDocument()
     },
-    rolePermission: ["Speaker of the Federalist Congress"], // TODO: Add the rest of the government roles
+    rolePermission: [
+        "Speaker of the Federalist Congress", 
+        "Members of the Chancellery", 
+        "Chief Justice",
+    ],
+    active: false,
 };
