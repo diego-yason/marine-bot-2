912b832955f73161cb0b55f1fd0028ec283b8af2
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-04-19 13:11:04 +0800

    moved commands folder to src

---
+++ build/commands/mod/kick.js
@@ -0,0 +1,3 @@
+module.exports = function (axios, data, res) {
+    console.log("Test!");
+};
---
+++ build/commands/ping.js
@@ -0,0 +1,4 @@
+module.exports = function (axios, data, res) {
+    res.reply("po||n||g?!");
+    console.log("made a reply!");
+};
